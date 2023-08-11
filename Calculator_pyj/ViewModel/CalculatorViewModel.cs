589d638510f59bbf4999fdfd6bf8855566d34d9b
yyujjinn <yujin@ati2000.co.kr>
2023-08-11 15:24:08 +0900

    Day 5 Model, ViewModel 분리

--- Calculator_pyj/ViewModel/CalculatorViewModel.cs
+++ Calculator_pyj/ViewModel/CalculatorViewModel.cs
@@ -220,23 +220,24 @@ namespace Calculator.ViewModel
         */
         private void executeEqualCommand(object parameter)
         {
+            CalculatorModel calModel = new CalculatorModel();
             if (selectedOperator != SelectedOperator.None && double.TryParse(Result, out double numericResult))
             {
                 switch (selectedOperator)
                 {
                     case SelectedOperator.Addiction:
-                        numericResult = SimpleMath.Add(lastNumber, numericResult);
+                        numericResult = calModel.Add(lastNumber, numericResult);
                         break;
                     case SelectedOperator.Substraction:
-                        numericResult = SimpleMath.Subtract(lastNumber, numericResult);
+                        numericResult = calModel.Subtract(lastNumber, numericResult);
                         break;
                     case SelectedOperator.Multiplication:
-                        numericResult = SimpleMath.Muliple(lastNumber, numericResult);
+                        numericResult = calModel.Muliple(lastNumber, numericResult);
                         break;
                     case SelectedOperator.Division:
                         if (numericResult != 0)
                         {
-                            numericResult = SimpleMath.Divide(lastNumber, numericResult);
+                            numericResult = calModel.Divide(lastNumber, numericResult);
                         }
                         else
                         {
@@ -257,59 +258,6 @@ namespace Calculator.ViewModel
             }
         }
 
-        #endregion
-
-        #region [클래스]
-        public class SimpleMath
-        {
-            /**
-            * @brief 숫자를 더하는 함수
-            * @param n1: 첫 번째 숫자, n2: 두 번째 숫자
-            * @return n1 + n2: 두 숫자의 합
-            * @note Patch-notes
-            * 2023-08-10 | 박유진 | 첫 번째 입력 값과 두 번째 입력 값을 더해줌
-            */
-            public static double Add(double n1, double n2)
-            {
-                return n1 + n2;
-            }
-
-            /**
-            * @brief 숫자를 빼는 함수
-            * @param n1: 첫 번째 숫자, n2: 두 번째 숫자
-            * @return n1 - n2: 첫 번째 숫자에서 두 번째 숫자를 뺀 값
-            * @note Patch-notes
-            * 2023-08-10 | 박유진 | 첫 번째 입력 값에서 두 번째 입력 값을 빼줌
-            */
-            public static double Subtract(double n1, double n2)
-            {
-                return n1 - n2;
-            }
-
-            /**
-            * @brief 숫자를 곱하는 함수
-            * @param n1: 첫 번째 숫자, n2: 두 번째 숫자
-            * @return n1 * n2: 두 숫자의 곱
-            * @note Patch-notes
-            * 2023-08-10 | 박유진 | 첫 번째 입력 값과 두 번째 입력 값을 곱해줌
-            */
-            public static double Muliple(double n1, double n2)
-            {
-                return n1 * n2;
-            }
-
-            /**
-            * @brief 숫자를 나누는 함수
-            * @param n1: 첫 번째 숫자, n2: 두 번째 숫자
-            * @return n1 / n2: 첫 번째 숫자에서 두 번째 숫자를 나눈 값
-            * @note Patch-notes
-            * 2023-08-10 | 박유진 | 첫 번째 입력 값에서 두 번째 입력 값을 나눠줌
-            */
-            public static double Divide(double n1, double n2)
-            {
-                return n1 / n2;
-            }
-        }
         #endregion
 
     }
